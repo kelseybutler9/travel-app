f08f83ee670ac9470f816c03ef48242253cb2557
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2017-10-18 16:43:26 -0500

    update to manage async operations

--- test/test-server.js
+++ test/test-server.js
@@ -1,21 +1,39 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../server.js');
+// var server = require('../server.js');
+const {app,runServer,closeServer} = require('../server.js');
 
 var should = chai.should();
-var app = server.app;
-var storage = server.storage;
+// var app = server.app;
+// var storage = server.storage;
 
 chai.use(chaiHttp);
 
-describe('initial page', function() {
-  it('exists', function(done) {
-    chai.request(app).get('/')
-      .end(function(err, res) {
-        console.log('Response:'+ res);
+describe('Users', function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closServer();
+  });
+
+  it('should exist on GET', function() {
+    return chai.request(app).get('/')
+      .then(function(res) {
         res.should.have.status(200);
         res.should.be.html;
-        done();
       });
   });
+
+// describe('initial page', function() {
+//   it('exists', function(done) {
+//     chai.request(app).get('/')
+//       .end(function(err, res) {
+//         console.log('Response:'+ res);
+//         res.should.have.status(200);
+//         res.should.be.html;
+//         done();
+//       });
+// });
 });
