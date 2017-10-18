ee1e40709983abad0a5811ef8e05bd29adc4515c
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2017-10-18 13:23:44 -0500

    add files

---
+++ test/test-server.js
@@ -0,0 +1,20 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server.js');
+
+var should = chai.should();
+var app = server.app;
+var storage = server.storage;
+
+chai.use('chaiHttp');
+
+describe('initial page', function() {
+  it('exists', function(done) {
+    chai.request(app).get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+      });
+  });
+});
