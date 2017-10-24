b8cd70d87849a4f90b0114c3328cd6c63bebaa41
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2017-10-23 21:46:19 -0500

    add in new files and folder

---
+++ public/app.js
@@ -0,0 +1,168 @@
+let MOCK_TRIP_UPDATES = {
+    "trips": [
+        {
+            "id": "1111111",
+            "title": "Vacation",
+            "location": "Chicago, Illinois",
+            "startDate": 10201992,
+            "endDate": 10301992,
+            "travel": [{
+              type: "Flight",
+              travelInformation: "Afternoon flight, $700" }],
+            "residence": [{
+              stay: "Hilton Inn",
+              residenceComments: "nice staff, clean room"}],
+            "restaurants": [{
+              name: "Taco Bell",
+              comments: "great burritos" }],
+            "activities": [{
+              name: "Surfing",
+              activtiyInformation: "Fun!"
+            }]
+        },
+        {
+            "id": "1111111",
+            "title": "Vacation",
+            "location": "Chicago, Illinois",
+            "startDate": 10201992,
+            "endDate": 10301992,
+            "travel": [{
+              type: "Flight",
+              travelInformation: "Afternoon flight, $700" }],
+            "residence": [{
+              stay: "Hilton Inn",
+              residenceComments: "nice staff, clean room"}],
+            "restaurants": [{
+              name: "Taco Bell",
+              comments: "great burritos" }],
+            "activities": [{
+              name: "Surfing",
+              activtiyInformation: "Fun!"
+            }]
+        },
+        {
+            "id": "1111111",
+            "title": "Vacation",
+            "location": "Chicago, Illinois",
+            "startDate": 10201992,
+            "endDate": 10301992,
+            "travel": [{
+              type: "Flight",
+              travelInformation: "Afternoon flight, $700" }],
+            "residence": [{
+              stay: "Hilton Inn",
+              residenceComments: "nice staff, clean room"}],
+            "restaurants": [{
+              name: "Taco Bell",
+              comments: "great burritos" }],
+            "activities": [{
+              name: "Surfing",
+              activtiyInformation: "Fun!"
+            }]
+        },
+        {
+            "id": "1111111",
+            "title": "Vacation",
+            "location": "Chicago, Illinois",
+            "startDate": 10201992,
+            "endDate": 10301992,
+            "travel": [{
+              type: "Flight",
+              travelInformation: "Afternoon flight, $700" }],
+            "residence": [{
+              stay: "Hilton Inn",
+              residenceComments: "nice staff, clean room"}],
+            "restaurants": [{
+              name: "Taco Bell",
+              comments: "great burritos" }],
+            "activities": [{
+              name: "Surfing",
+              activtiyInformation: "Fun!"
+            }]
+        }
+    ]
+};
+
+// function getData() {
+//   const settings ={
+//     url: '/trips',
+//     data: {
+//
+//     }
+//
+//   }
+//
+// }
+
+function getPastTrips(callbackFn) {
+    setTimeout(function(){ callbackFn(MOCK_TRIP_UPDATES)}, 100);
+}
+
+function displayTrips(data) {
+    for (index in data.trips) {
+       $('past-trips-list').append(
+        '<p>' + data.trips[index].text + '</p>');//update this to be a past trip
+    }
+}
+
+function getAndDisplayTrips() {
+    getPastTrips(displayTrips);
+}
+
+$(function() {
+    getAndDisplayTrips();
+})
+
+$('.homescreen').on('submit', function(event) {
+	event.preventDefault();
+  createNewTripForm();
+	$('.homescreen').prop('hidden', true);
+});
+
+function createNewTripForm () {
+  $('.create-a-trip-form').prop('hidden', false);
+}
+
+$('.travel').on('submit', function(event) {
+	event.preventDefault();
+  createNewTravelItem();
+});
+
+$('.residence').on('submit', function(event) {
+	event.preventDefault();
+  createNewResidenceItem();
+});
+
+$('.restaurants').on('submit', function(event) {
+	event.preventDefault();
+  createNewRestaurantItem();
+});
+
+$('.activities').on('submit', function(event) {
+	event.preventDefault();
+  createNewActivityItem();
+});
+
+function createNewTravelItem() {
+  $('.travel-html').append(`<label>Enter the type of transportation for this trip.
+    <input type="text" name="airport"></label><label>Enter more information about your travel.
+    <input type="text" name="flight-information"></label>`);
+}
+
+function createNewResidenceItem() {
+  $('.residence-html').append(`<label>Enter the place you stayed at during your trip.
+    <input type="text" name="hotel"></label><label>Enter more information about your place of stay.
+    <input type="text" name="hotel-information"></label>`);
+}
+
+function createNewRestaurantItem() {
+  $('.restaurants-html').append(`<label>Enter a restaurant you visited during your trip.
+    <input type="text" name="restaurant"></label><label>Enter more information about the restaurant.
+    <input type="text" name="restaurant-information"></label>`);
+}
+
+function createNewActivityItem() {
+  $('.activities-html').append(`<label>Enter an activtiy you did during your trip.
+    <input type="text" name="activity"></label><label>Enter more information about the activity.
+    <input type="text" name="activity-information"></label>`);
+}
