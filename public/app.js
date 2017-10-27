a01f19825bd0a9a19abc7620f376e160cd89b506
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2017-10-26 22:08:50 -0500

    add in past trip functionality to view the past trips list and update a particular trip

--- public/app.js
+++ public/app.js
@@ -7,14 +7,14 @@ let MOCK_TRIP_UPDATES = {
             "startDate": 10201992,
             "endDate": 10301992,
             "travel": [{
-              type: "Flight",
+              travelType: "Flight",
               travelInformation: "Afternoon flight, $700" }],
             "residence": [{
               stay: "Hilton Inn",
               residenceComments: "nice staff, clean room"}],
             "restaurants": [{
               restaurantName: "Taco Bell",
-              comments: "great burritos" }],
+              restaurantComments: "great burritos" }],
             "activities": [{
               activityName: "Surfing",
               activtiyInformation: "Fun!"
@@ -27,14 +27,14 @@ let MOCK_TRIP_UPDATES = {
             "startDate": 10201992,
             "endDate": 10301992,
             "travel": [{
-              type: "Flight",
+              travelType: "Flight",
               travelInformation: "Afternoon flight, $700" }],
             "residence": [{
               stay: "Hilton Inn",
               residenceComments: "nice staff, clean room"}],
             "restaurants": [{
               restaurantName: "Taco Bell",
-              comments: "great burritos" }],
+              restaurantComments: "great burritos" }],
             "activities": [{
               activityName: "Surfing",
               activtiyInformation: "Fun!"
@@ -47,14 +47,14 @@ let MOCK_TRIP_UPDATES = {
             "startDate": 10201992,
             "endDate": 10301992,
             "travel": [{
-              type: "Flight",
+              travelType: "Flight",
               travelInformation: "Afternoon flight, $700" }],
             "residence": [{
               stay: "Hilton Inn",
               residenceComments: "nice staff, clean room"}],
             "restaurants": [{
               restaurantName: "Taco Bell",
-              comments: "great burritos" }],
+              restaurantComments: "great burritos" }],
             "activities": [{
               activityName: "Surfing",
               activtiyInformation: "Fun!"
@@ -99,15 +99,114 @@ function getPastTrips(callbackFn) {
 }
 
 function displayPastTrips(data) {
+    $('.past-trips').html('');
     for (index in data.trips) {
       console.log(data.trips[index]);
-      $('.past-trips').append(`<ls class="past-trip"><h3>${data.trips[index].title}<h3><p>${data.trips[index].location}</p><p>${data.trips[index].startDate}</p><p>${data.trips[index].endDate}</p></ls>`);
+      $('.past-trips').append(`<ls id="${index}"class="past-trip"><button type="button" id="${data.trips[index].id} "class="update-trip-button">${data.trips[index].title}</button><p>${data.trips[index].location}</p><p>${data.trips[index].startDate}</p><p>${data.trips[index].endDate}</p></ls>`);
     }
 }
 
 function getAndDisplayTrips() {
     getPastTrips(displayPastTrips);
 }
+
+function getUpdatePastTrip(callbackFn, tripId) {
+
+  setTimeout(function() {callbackFn(MOCK_TRIP_UPDATES, tripId)}, 100);
+}
+
+function displayPastTrip(data, tripId) {
+  $('.create-a-trip-form').prop('hidden', true);
+  $('.update-past-trip-form').prop('hidden', false);
+  $(`.homescreen`).prop('hidden', true);
+  $(`.past-trips-list`).prop('hidden', true);
+	$('.homescreen').prop('hidden', true);
+  //let identifiedTrip = data.trips.map(trip => {if(trip.id === tripId) { return trip}});
+  //let index = data.trips.find(trip => trip.id === 'tripId');
+  //console.log(index + ": index");
+  $('.update-past-trip-form').html('');
+  let travelDetails;
+  data.trips[tripId].travel.forEach(function (travel) {
+    travelDetails += `<label>Enter the type of transportation for this trip.
+      <input type="text" name="airport" value=${travel.travelType}>
+    </label>
+    <label>Enter more information about your travel.
+      <input type="text" name="flight-information" value=${travel.travelInformation}>
+    </label>`
+  });
+  console.log(travelDetails);
+  //  $('.update-past-trip-form').append(`
+  //    <h3>General Info</h3>
+  //  <form class="general-info">
+  //    <div class="general-info-html">
+  //      <label>Enter the title of your trip.
+  //        <input type="text" name="title" value=${data.trips[tripId].title}>
+  //      </label>
+  //      <label>Enter the location of your trip.
+  //        <input type="text" name="location" value=${data.trips[tripId].location}>
+  //      </label>
+  //      <label>Enter the start date of your trip.
+  //        <input type="text" name="start-date" value=${data.trips[tripId].startDate}>
+  //      </label>
+  //      <label>Enter the end date of your trip.
+  //        <input type="text" name="end-date" value=${data.trips[tripId].endDate}>
+  //      </label>
+  //    </div>
+  //  </form>
+  //  <h3>Travel</h3>
+  //  <form class="travel">
+  //    <div class="travel-html">
+  //       ${travelDetails}
+  //    </div>
+  //    <button type="submit">Add another travel item</button>
+  //  </form>
+  //  <h3>Residence</h3>
+  //  <form class="residence">
+  //    <div class="residence-html">
+  //      <label>Enter the place you stayed at during your trip.
+  //        <input type="text" name="hotel" value=${data.trips[tripId].residence}>
+  //      </label>
+  //      <label>Enter more information about your place of stay.
+  //        <input type="text" name="hotel-information" value=${data.trips[tripId].residenceComments}>
+  //      </label>
+  //    </div>
+  //    <button type="submit">Add another residence item</button>
+  //  </form>
+  //  <h3>Restaurants</h3>
+  //  <form class="restaurants">
+  //    <div class="restaurants-html">
+  //      <label>Enter a restaurant you visited during your trip.
+  //        <input type="text" name="restaurant" value=${data.trips[tripId].restaurantName}>
+  //      </label>
+  //      <label>Enter more information about the restaurant.
+  //        <input type="text" name="restaurant-information" value=${data.trips[tripId].restaurantComments}>
+  //      </label>
+  //    </div>
+  //    <button type="submit">Add another restaurant</button>
+  //  </form>
+  //  <h3>Activties</h3>
+  //  <form class="activities">
+  //    <div class="activities-html">
+  //      <label>Enter an activtiy you did during your trip.
+  //        <input type="text" name="activity" value=${data.trips[tripId].activityName}>
+  //      </label>
+  //      <label>Enter more information about the activity.
+  //        <input type="text" name="activity-information" value=${data.trips[tripId].activtiyInformation}>
+  //      </label>
+  //    </div>
+  //    <button type="submit">Add another activity</button>
+  //  </form>`);
+}
+
+function getAndDisplayPastTrip(tripId) {
+    getUpdatePastTrip(displayPastTrip, tripId);
+}
+
+$('.past-trips').on('click', '.update-trip-button', function(event) {
+  let tripId = this.parentElement.id;
+  viewPastTrip(tripId);
+})
+
 
 $('.new-trip-button').click(function(event) {
 	event.preventDefault();
@@ -216,6 +315,10 @@ function addTripToDatabase() {
 function viewPastTripsList() {
   console.log('view past trips list');
   getAndDisplayTrips();
+}
+
+function viewPastTrip(tripId) {
+  getAndDisplayPastTrip(tripId);
 }
 
 function updateTripInDatabase() {
