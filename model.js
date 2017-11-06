d9f082a5ebae009ac7520ea5e493b26364e74184
kelseybutler9 <30805822+kelseybutler9@users.noreply.github.com>
2017-11-06 15:25:45 -0600

    update array parsing for schema

--- model.js
+++ model.js
@@ -20,30 +20,42 @@ const tripSchema = mongoose.Schema({
 });
 
 tripSchema.methods.apiRepr = function() {
-
+  let transArray = [];
+    let residenceArray = [];
+    let restArray = [];
+    let activityArray = [];
+    function parseArrays(array, object, firstKey, secondKey) {
+//       this.object.forEach(function (item) {
+                array.push({firstKey: this.item.firstKey, secondKey: this.item.secondKey});
+        }) 
+        return array;
+          
+// }
   return {
     id: this._id,
     title: this.title,
     place: this.place,
     startDate: this.startDate,
     endDate: this.endDate,
-    transportation: [{transType: this.transportation[0].transType, transInformation: this.transportation[0].transInfromation }],
-    residence: [{
-              residenceName: this.residence.residenceName,
-              residenceInformation: this.residence.residenceInformation}],
-    restaurants: [{
-              restaurantName: this.restaurants.restaurantName,
-              restaurantInformation: this.restaurants.restaurantInformation}],
-    activities: [{
-              activityName: this.activities.activityName,
-              activtiyInformation: this.activities.activityInformation
-                }]
+    //transportation: [{transType: this.transportation[0].transType, transInformation: this.transportation[0].transInfromation }],
+    //residence: [{
+              //residenceName: this.residence.residenceName,
+             // residenceInformation: this.residence.residenceInformation}],
+    //restaurants: [{
+              //restaurantName: this.restaurants.restaurantName,
+             // restaurantInformation: this.restaurants.restaurantInformation}],
+    //activities: [{
+             // activityName: this.activities.activityName,
+              //activtiyInformation: this.activities.activityInformation
+              //  }]
+      transportation: parseArrays(transArray, "transportation", "transType", "transInformation"),
+      residence: parseArrays(residenceArray, "residence", "residenceName", "residenceInformation"),
+      restaurants: parseArrays(restArray, "restaurants", "restaurantName", "restaurantInformation"),
+      activities: parseArrays(activityArray, "activities", "activityName", "activityInformation")
   };
 }
 
-// function parseArrays() {
-//
-// }
+
 
 const Trip = mongoose.model('Trip', tripSchema);
 
