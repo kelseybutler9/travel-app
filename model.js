9efa74037679bcf5b37c70f415d7efd3e0b85ac1
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2017-11-06 21:47:08 -0600

    finalize and refactor model to make a successful get call

--- model.js
+++ model.js
@@ -20,39 +20,28 @@ const tripSchema = mongoose.Schema({
 });
 
 tripSchema.methods.apiRepr = function() {
-    let transArray = [];
-    let residenceArray = [];
-    let restArray = [];
-    let activityArray = [];
-    function parseArrays(array, object, firstKey, secondKey) {
-//       this.object.forEach(function (item) {
-          console.log(array);
-                array.push({firstKey: this.item.firstKey, secondKey: this.item.secondKey});
-        }
-        return array;
-
-// }
-  return {
-    id: this._id,
-    title: this.title,
-    place: this.place,
-    startDate: this.startDate,
-    endDate: this.endDate,
-    //transportation: [{transType: this.transportation[0].transType, transInformation: this.transportation[0].transInfromation }],
-    //residence: [{
-              //residenceName: this.residence.residenceName,
-             // residenceInformation: this.residence.residenceInformation}],
-    //restaurants: [{
-              //restaurantName: this.restaurants.restaurantName,
-             // restaurantInformation: this.restaurants.restaurantInformation}],
-    //activities: [{
-             // activityName: this.activities.activityName,
-              //activtiyInformation: this.activities.activityInformation
-              //  }]
-      transportation: parseArrays(transArray, "transportation", "transType", "transInformation"),
-      residence: parseArrays(residenceArray, "residence", "residenceName", "residenceInformation"),
-      restaurants: parseArrays(restArray, "restaurants", "restaurantName", "restaurantInformation"),
-      activities: parseArrays(activityArray, "activities", "activityName", "activityInformation")
+    function parseArrays(topic, firstKey, secondKey) {
+          let itemArray = [];
+          for(i=0; i< topic.length; i++) {
+              let item = topic[i];
+              let newObject = {};
+              newObject[firstKey] = item[firstKey];
+              newObject[secondKey] = item[secondKey];
+              console.log(newObject);
+              itemArray.push(newObject);
+          }
+        return itemArray;
+      }
+    return {
+      id: this._id,
+      title: this.title,
+      place: this.place,
+      startDate: this.startDate,
+      endDate: this.endDate,
+      transportation: parseArrays(this.transportation, "transType", "transInformation"),
+      residence: parseArrays(this.residence, "residenceName", "residenceInformation"),
+      restaurants: parseArrays(this.restaurants, "restaurantName", "restaurantInformation"),
+      activities: parseArrays(this.activities, "activityName", "activityInformation")
   };
 }
 
