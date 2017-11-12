b13c7da89855b53e04371a05efce39f721bc62e1
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2017-11-12 11:12:01 -0600

    update formatting

--- public/datastore.js
+++ public/datastore.js
@@ -1,72 +1,65 @@
 (function (exports) {
-
   function DataStore (url) {
     this.url = url;
-    if(!url) {
+    if (!url) {
       throw new Error('no endpoint provided');
     }
   }
 
   DataStore.prototype.add = function (item) {
-      console.log(item);
-      $.ajax({
-           method: 'POST',
-           url: `${this.url}`,
-           data: JSON.stringify(item),
-           success: function() {
-               console.log('success');
-           },
-           dataType: 'json',
-           contentType: 'application/json'
-      });
-  }
+    $.ajax({
+      method: 'POST',
+      url: this.url,
+      data: JSON.stringify(item),
+      success: function () {
+        console.log('success');
+      },
+      dataType: 'json',
+      contentType: 'application/json'
+    });
+  };
 
   DataStore.prototype.get = function (id, callback) {
-    $.get(`${this.url}/${id}`, function(res) {
+    $.get(`${this.url}/${id}`, function (res) {
       console.log(res);
       callback(res);
     });
-  }
+  };
 
   DataStore.prototype.getAll = function (callback) {
-    $.getJSON(`${this.url}`, function(res) {
-        console.log(res);
-        callback(res);
+    $.getJSON(this.url, function (res) {
+      console.log(res);
+      callback(res);
     });
-  }
+  };
 
   DataStore.prototype.remove = function (item, id) {
-      console.log('delete data in database');
-      $.ajax({
-           method: 'DELETE',
-           url: `${this.url}/${id}`,
-           data: JSON.stringify(item),
-           success: function() {
-               $(".delete-screen").prop("hidden", false);
-           },
-           dataType: 'json',
-           contentType: 'application/json'
-      });
-  }
+    console.log('delete data in database');
+    $.ajax({
+      method: 'DELETE',
+      url: `${this.url}/${id}`,
+      data: JSON.stringify(item),
+      success: function () {
+        $('.delete-screen').prop('hidden', false);
+      },
+      dataType: 'json',
+      contentType: 'application/json'
+    });
+  };
 
   DataStore.prototype.update = function (id, item, callback) {
-      console.log('update data in database');
-      // $.put(`${this.url}/${id}`, item, function(res) {
-      // console.log(res);
-      // callback(res);
-      // });
-
-      $.ajax({
-           method: 'PUT',
-           url: `${this.url}/${id}`,
-           data: JSON.stringify(item),
-           success: function() {
-               console.log('successful update');
-           },
-           dataType: 'json',
-           contentType: 'application/json'
-      });
-   }
+    console.log('update data in database');
+    $.ajax({
+      method: 'PUT',
+      url: `${this.url}/${id}`,
+      data: JSON.stringify(item),
+      success: function () {
+        console.log('successful update');
+      },
+      dataType: 'json',
+      contentType: 'application/json'
+    });
+  };
 
   exports.DataStore = DataStore;
-})(typeof exports === 'undefined' ? window.app : exports)
+})(typeof exports === 'undefined' ? window.app : exports);
