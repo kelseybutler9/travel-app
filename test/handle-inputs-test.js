e6626983517417c4f6d557836c19042745834f96
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2017-11-26 12:11:58 -0600

    update handleInputs test files

--- test/handle-inputs-test.js
+++ test/handle-inputs-test.js
@@ -1,12 +1,50 @@
 const handleInputs = require('../public/handle-inputs');
 const chai = require('chai');
+require('chai').should();
 
 const should = chai.should();
 
-describe('Test', function () {
+describe('handleInputs', function () {
 
-  it('should print handleInputs', function () {
+  it('should return a trip object', function () {
+    const data =
+    {
+      {name:'title',value:'New Trip'},
+      {name:'place',value:'Chicago'},
+      {name:'startDate',value:'October 2017'},
+      {name:'endDate',value:'November 2017'},
+      {name:'transType',value:'Flight'},
+      {name:'transInformation',value:'Afternoon, 3pm'}
+      {name:'residenceName',value:'Parents House'}
+      {name:'residenceInformation',value:'Cozy'}
+      {name:'restaurantName',value:'Hunans'}
+      {name:'restaurantInformation',value:'Chinese Food'}
+      {name:'activityName',value:'Sledding'}
+      {name:'activityInformation',value:'Fun!'}
+    };
 
-console.log('-------------------', handleInputs);
+    const trip = handleInputs(data);
+
+    trip.should.be.an('object');
+    trip.should.have.length.of.at.least(1);
+    trip.should.include.keys('title', 'place', 'startDate', 'endDate', 'transportation', 'residence', 'restaurants', 'activities');
+    trip.title.should.be.a('string');
+    trip.place.should.be.a('string');
+    trip.startDate.should.be.a('string');
+    trip.endDate.should.be.a('string');
+    trip.transportation.should.be.a('array');
+    trip.transportation.should.have.length.of.at.least(1);
+    trip.residence.should.be.a('array');
+    trip.residence.should.have.length.of.at.least(1);
+    trip.restaurants.should.be.a('array');
+    trip.restaurant.should.have.length.of.at.least(1);
+    trip.activities.should.be.a('array');
+    trip.activities.should.have.length.of.at.least(1);
+
+    trip.transportation.should.include.keys('transType', 'transInformation');
+    trip.residence.should.include.keys('residenceName', 'residenceInformation');
+    trip.restaurants.should.include.keys('restaurantName', 'restaurantInformation');
+    trip.activities.should.include.keys('activityName', 'activityInformation');
+
   });
 });
